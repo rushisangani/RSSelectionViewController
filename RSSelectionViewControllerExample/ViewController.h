43acbd8cdea9d37430b1944363bbf35d882e99d2
Rushi Sangani <rushisangani@gmail.com>
2016-06-12 13:54:42 +0530

    Initial Commit

---
+++ RSSelectionViewControllerExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSelectionViewControllerExample
+//
+//  Created by Rushi Sangani on 12/06/16.
+//  Copyright © 2016 Rushi Sangani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RSSelectionViewControllerExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RSSelectionViewControllerExample
+//
+//  Created by Rushi Sangani on 12/06/16.
+//  Copyright © 2016 Rushi Sangani. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
